d6d353afa47cdac226ec8296c78df98c9656a938
João Filipe <joaofilipeb045@gmail.com>
2023-11-17 08:20:21 -0300

    Controller Pacientes

---
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,51 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.paciente.DadosAtualizacaoPaciente;
+import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pacientes")
+public class PacienteController {
+
+    private PacienteRepository pacienteRepository;
+
+    public PacienteController(PacienteRepository repository) {
+        this.pacienteRepository = repository;
+    }
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        pacienteRepository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
+        return pacienteRepository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+    }
+''
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+        var paciente = pacienteRepository.getReferenceById(dados.id());
+        paciente.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        var paciente = pacienteRepository.getReferenceById(id);
+        paciente.excluir();
+    }
+
+}
