3569ae877f7edb7d3676234f773d51ebf0f698e6
João Filipe <joaofilipeb045@gmail.com>
2023-11-20 08:08:11 -0300

    Service de autenticação

---
+++ src/main/java/med/voll/api/service/AutenticacaoService.java
@@ -0,0 +1,24 @@
+package med.voll.api.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import med.voll.api.repository.UsuarioRepository;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    private UsuarioRepository usuarioRepository;
+
+    public AutenticacaoService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByLogin(username);
+    }
+
+}
