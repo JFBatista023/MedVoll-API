912e1b9c8cc5ac212d0882823bec9d2ed6389fab
João Filipe <filipeb@sefaz.pi.gov.br>
2023-11-28 11:26:18 -0300

    Chamando método de agendamento

--- src/main/java/med/voll/api/web/controller/ConsultaController.java
+++ src/main/java/med/voll/api/web/controller/ConsultaController.java
@@ -10,14 +10,23 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.dto.consulta.DadosAgendamentoConsulta;
 import med.voll.api.dto.consulta.DadosDetalhamentoConsulta;
+import med.voll.api.service.ConsultaService;
 
 @RestController
 @RequestMapping("/consultas")
 public class ConsultaController {
 
+    private ConsultaService consultaService;
+
+    public ConsultaController(ConsultaService consultaService) {
+        this.consultaService = consultaService;
+    }
+
     @PostMapping
     @Transactional
     public ResponseEntity<DadosDetalhamentoConsulta> agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
-        return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
+        consultaService.agendar(dados);
+        return ResponseEntity.ok().build();
     }
+
 }
