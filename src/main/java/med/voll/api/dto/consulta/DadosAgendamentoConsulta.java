36c9a8a85be9da04e7e2f2278abbe431e731812d
João Filipe <filipeb@sefaz.pi.gov.br>
2023-11-28 11:23:33 -0300

    Add especialidade

--- src/main/java/med/voll/api/dto/consulta/DadosAgendamentoConsulta.java
+++ src/main/java/med/voll/api/dto/consulta/DadosAgendamentoConsulta.java
@@ -4,10 +4,12 @@ import java.time.LocalDateTime;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.enums.Especialidade;
 
 public record DadosAgendamentoConsulta(
-        Long idMedico,
-        @NotNull Long idPaciente,
-        @NotNull @Future LocalDateTime data) {
+                Long idMedico,
+                @NotNull Long idPaciente,
+                @NotNull @Future LocalDateTime data,
+                Especialidade especialidade) {
 
 }
